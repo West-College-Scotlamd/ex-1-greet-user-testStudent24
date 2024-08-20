608b70cff52b20427b9058b959e9dabea4a552ac
testStudent24 <jud.nelson@gmail.com>
2024-08-20 16:43:13 +0000

    added !

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def greet_user(name):
     # TODO: return a greeting string "Hello, <name>!"
-    return "Hello, "+ name
+    return "Hello, "+ name+"!"
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
